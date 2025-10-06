cac4d6f8bbd6c43d2d8bb889d5fefcfed6de2930
ManojramRagu <manojramragu@gmail.com>
2025-10-06 22:56:30 +0530

    Updated promotion carousel to accomodate for it in menu page

--- lib/widgets/promo_carousel.dart
+++ lib/widgets/promo_carousel.dart
@@ -1,12 +1,18 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
+typedef SlideOverlayBuilder = Widget Function(BuildContext context, int index);
+
 class PromoCarousel extends StatefulWidget {
   final List<String> imagePaths;
   final double height;
   final Duration interval;
   final Duration slideDuration;
   final bool dotsBelow;
+
+  final void Function(int index)? onTapSlide;
+  final void Function(int index)? onIndexChanged;
+  final SlideOverlayBuilder? overlayBuilder;
 
   const PromoCarousel({
     super.key,
@@ -15,6 +21,9 @@ class PromoCarousel extends StatefulWidget {
     this.interval = const Duration(seconds: 3),
     this.slideDuration = const Duration(milliseconds: 350),
     this.dotsBelow = true,
+    this.onTapSlide,
+    this.onIndexChanged,
+    this.overlayBuilder,
   });
 
   @override
@@ -36,6 +45,7 @@ class _PromoCarouselState extends State<PromoCarousel> {
 
   void _startAutoPlay() {
     _timer?.cancel();
+    if (widget.imagePaths.isEmpty) return;
     _timer = Timer.periodic(widget.interval, (_) {
       if (!_isUserInteracting) _goNext();
     });
@@ -72,7 +82,7 @@ class _PromoCarouselState extends State<PromoCarousel> {
   Widget build(BuildContext context) {
     final cs = Theme.of(context).colorScheme;
 
-    // Dots per promotion
+    // Dots
     Widget _dots() => Row(
       mainAxisSize: MainAxisSize.min,
       children: List.generate(widget.imagePaths.length, (i) {
@@ -95,12 +105,18 @@ class _PromoCarouselState extends State<PromoCarousel> {
       }),
     );
 
-    // Arrows to slide
+    // Slide stack
     final slide = SizedBox(
       height: widget.height,
       child: Listener(
-        onPointerDown: (_) => _isUserInteracting = true,
-        onPointerUp: (_) => _isUserInteracting = false,
+        onPointerDown: (_) {
+          _isUserInteracting = true;
+          _timer?.cancel();
+        },
+        onPointerUp: (_) {
+          _isUserInteracting = false;
+          _startAutoPlay();
+        },
         child: Stack(
           alignment: Alignment.center,
           children: [
@@ -108,15 +124,25 @@ class _PromoCarouselState extends State<PromoCarousel> {
               borderRadius: BorderRadius.circular(14),
               child: PageView.builder(
                 controller: _controller,
-                onPageChanged: (i) => setState(() => _index = i),
+                onPageChanged: (i) {
+                  setState(() => _index = i);
+                  widget.onIndexChanged?.call(i);
+                },
                 itemCount: widget.imagePaths.length,
-                itemBuilder: (_, i) => Image.asset(
-                  widget.imagePaths[i],
-                  fit: BoxFit.cover,
-                  width: double.infinity,
+                itemBuilder: (_, i) => GestureDetector(
+                  onTap: widget.onTapSlide == null
+                      ? null
+                      : () => widget.onTapSlide!(i),
+                  child: Image.asset(
+                    widget.imagePaths[i],
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  ),
                 ),
               ),
             ),
+
+            // Chevron arrows
             Positioned(
               left: 8,
               child: _RoundIconButton(
@@ -135,6 +161,15 @@ class _PromoCarouselState extends State<PromoCarousel> {
                 fg: cs.onSurface,
               ),
             ),
+
+            if (widget.overlayBuilder != null)
+              Positioned(
+                left: 12,
+                right: 12,
+                bottom: 10,
+                child: widget.overlayBuilder!(context, _index),
+              ),
+
             if (!widget.dotsBelow)
               Positioned(
                 bottom: 8,
@@ -163,7 +198,6 @@ class _PromoCarouselState extends State<PromoCarousel> {
         children: [slide, const SizedBox(height: 8), _dots()],
       );
     }
-
     return slide;
   }
 }
